49c516d2b8b1e5419e94162e7954db4becae0c5c
Francois Gauthier-Clerc <francois@gauthier-clerc.fr>
2018-12-28 22:25:44 +0100

    fix mistake and add features.

--- model/action.py
+++ model/action.py
@@ -9,11 +9,17 @@ class Action:
         self.game = game
         self.type=dtype
 
-    def do(self):
-        self.game.__getattribute__(self.type)(*(self.init_pos+self.final_pos ))
+    def do(self, board=None):
+        if board is None:
+            self.game.__getattribute__(self.type)(*(self.init_pos+self.final_pos ))
+        else:
+            board.__getattribute__(self.type)(*(self.init_pos+self.final_pos ))
 
-    def undo(self):
-        self.game.__getattribute__(self.type)(*(self.final_pos+self.init_pos ), force=True)
+    def undo(self, board=None):
+        if board is None:
+            self.game.__getattribute__(self.type)(*(self.final_pos+self.init_pos ), force=True)
+        else:
+            board.__getattribute__(self.type)(*(self.final_pos+self.init_pos ), force=True)
         
     def __repr__(self):
         return "<type="+self.type + " "+ str(self.init_pos) + " , "+ str(self.final_pos) +" at "+hex(id(self))+">"
--- model/board.py
+++ model/board.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from action import Action
+from model.action import Action
 
 
 HEIGHT = 7
@@ -42,7 +42,7 @@ class Board():
             return False
 
         #Verif des actions déjà faites
-        if self.state[0]>=2:
+        if self.state[0]>=3:
             return False
         
         #Application
@@ -65,32 +65,35 @@ class Board():
 
         # Verif si il y a pas de connerie entre les deux 
         coef_x = (x_final - x_init)
-        if coef_x!=0: coef_x /= abs((x_final - x_init))
-        
         coef_y = (y_final - y_init)
-        if coef_y!=0: coef_y/abs((y_final - y_init))
+
+        if coef_x!=0 and coef_y!=0 and abs(coef_x)!=abs(coef_y):
+            return False
+
+        if coef_x!=0: coef_x /= abs((x_final - x_init))
+        if coef_y!=0: coef_y/=abs((y_final - y_init))
 
         #print(coef_x, coef_y)
         #print(max(  y_final-y_init , x_final- x_init  ))
-        for i in range(max(  y_final-y_init , x_final- x_init  )-1):
-            if [x_init*i*coef_x, y_init*i*coef_y] in self.pawns[0] or [x_init*i*coef_x, y_init*i*coef_y] in self.pawns[1]: 
+        for i in range(max(  y_final-y_init , x_final- x_init  )-2):
+            if [x_init + (i+1)*coef_x, y_init + (i+1)*coef_y] in self.pawns[0] or [x_init + (i+1)*coef_x, y_init + (i+1)*coef_y] in self.pawns[1]: 
                 return False
         # Application
         self.balls[self.current_player] = [x_final, y_final]
-        self.state[1]+=1
+        self.state[0]+=1
         self.updatePlayer()
 
 
     def getActions(self):
         # Génération des actions movePawns
         actions = list()
-        if self.state[0]<2:
+        if self.state[0]<3:
             for [x,y] in self.pawns[self.current_player]:
                 # si le pion a la balle
 
                 if [x,y] in self.balls:
                     continue
-                    
+
                 for [x_d, y_d] in [1,0],[0,1],[-1,0],[0,-1]:
                     # Si la destination est hors zone
                     if  not ( (0<=x+x_d<WIDTH) and (0<=y+y_d<HEIGHT)):
@@ -104,23 +107,25 @@ class Board():
                     
                     actions.append(Action(self, x, y, x+x_d, y+y_d,dtype="movePawn"))
 
-        if self.state[1]<1:
+        if self.state[0]<3:
             x_ball, y_ball = self.balls[self.current_player]
             for [x, y] in self.pawns[self.current_player]:
                 # Si je suis le pion osef
                 if [x, y] == [x_ball, y_ball]:
                     continue
-                
                 # Deux cas de figure, diagonal ou linear
                 coef_x = (x - x_ball)
-                if coef_x!=0: coef_x /= abs((x - x_ball))
-                
                 coef_y = (y - y_ball)
-                if coef_y!=0: coef_y/abs((y - y_ball))
+
+                if coef_x!=0 and coef_y!=0 and abs(coef_x)!=abs(coef_y):
+                    continue
+
+                if coef_x!=0: coef_x /= abs((x - x_ball))
+                if coef_y!=0: coef_y /=abs((y - y_ball))
 
                 collided = False
-                for i in range(max(  y-y_ball , x- x_ball  )-1):
-                    if [x_ball*i*coef_x, y_ball*i*coef_y] in self.pawns[0] or [x_ball*i*coef_x, y_ball*i*coef_y] in self.pawns[1]: 
+                for i in range(max(  abs(y-y_ball) , abs(x- x_ball)  )-2):
+                    if [x_ball + (i+1)*coef_x, y_ball + (i+1)*coef_y] in self.pawns[0] or [x_ball + (i+1)*coef_x, y_ball + (i+1)*coef_y] in self.pawns[1]: 
                         collided = True
                         break
                 
@@ -130,10 +135,25 @@ class Board():
         return actions
 
 
+    def winner(self):
+        if self.balls[0][1] == HEIGHT-1:
+            return 1
+        if self.balls[1][1] == 0:
+            return -1
+        return 0
+
     def updatePlayer(self):
-        if self.state[0]>=2 and self.state[1]>=1:
+        if self.state[0]>=3:
             self.current_player = (self.current_player+1)%2
-            self.state = [0,0]
+            self.state = [0]
+
+    def copy(self):
+        bb = Board()
+        bb.state = self.state.copy()
+        bb.balls = self.balls.copy()
+        bb.pawns = self.pawns.copy()
+        bb.current_player = self.current_player
+        return bb
 
     def show(self):
         mat = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
@@ -150,12 +170,18 @@ class Board():
 
 
 if __name__ == "__main__":
+    from random import choice
     a = Board()
-    print(a.movePawn(2,0,2,1))
-    b = Action(a, 3,0,2,1,dtype="moveBall")
-    b.do()
-    a.show()
-    b.undo()
+    
+    i = 0
+    while a.winner() == -1:
+        try:
+            choice(a.getActions()).do()
+        except:
+            break
+        i += 1
+    print(i)
+    print(a.winner())
     a.show()
     #print(a.movePawn(1,0,1,1)) 
     #print(a.movePawn(0,0,0,1, force=True))
