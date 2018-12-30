088c944134e0aca8758193a3eecf3715560b12cc
Francois Gauthier-Clerc <francois@gauthier-clerc.fr>
2018-12-30 15:18:15 +0100

    add custom MCTS like alpha zero

--- mcts/tree.py
+++ mcts/tree.py
@@ -30,10 +30,12 @@ class Node:
 
 
 class Tree:
-    def __init__(self, init_board):
+    def __init__(self, init_board, ia, side):
         self.N = 0
-        self.init_board = init_board.copy()
+        self.init_board = deepcopy(init_board)
         self.root = Node(None, None, 0)
+        self.ia = ia
+        self.side = side # 1 = j1  |  -1 = j2
 
 
     def compute(self):
@@ -42,10 +44,13 @@ class Tree:
         # On cherche le noeud a explorer
         while cur_node.nb_visit!=0:
             childrens = cur_node.getChildren(cur_board)
+            actions = [children.action for children in childrens]
+            probas, _ = self.ia.eval(cur_board.get_board_raw(), cur_board.get_actions_board_raw(actions))
             weights = [0 for children in childrens]
             for i in range(len(childrens)):
                 try: 
-                    weights[i] = childrens[i].total_score/childrens[i].nb_visit + sqrt(2) * sqrt(log(cur_node.nb_visit)/childrens[i].nb_visit)
+                    weights[i]  = self.side*childrens[i].total_score/childrens[i].nb_visit  + probas[i]*sqrt(2) * sqrt(cur_node.nb_visit)/ (1+childrens[i].nb_visit)
+                    #weights[i] = childrens[i].total_score/childrens[i].nb_visit + sqrt(2) * sqrt(log(cur_node.nb_visit)/childrens[i].nb_visit)
                 except ZeroDivisionError:
                     weights[i] = inf
             
@@ -55,25 +60,20 @@ class Tree:
                 cur_node =  childrens[weights.index(max(weights))]
             #print(childrens)
             cur_node.apply(cur_board)
-
-        # On fait la simulation
-        while cur_board.winner() == 0:
-            try:
-                choice(cur_board.getActions()).do()
-            except:
-                break
-        score = cur_board.winner()
+        #TODO ce score n'est pas -1 ou 1 mais [-1,1]
+        score = self.ia.get_score(cur_board.get_board_raw())
         #ROOL BACK
         while not cur_node is None:
             cur_node.nb_visit+=1
             cur_node.total_score+=score
             cur_node = cur_node.parent          
 
+
     def get_best(self):
-        return max(self.root.childrens, key=lambda element: element.total_score/element.nb_visit).action
+        return max(self.root.childrens, key=lambda element: self.side*element.total_score/element.nb_visit).action
 
 
     def do_best(self, board):
-        action = max(self.root.childrens, key=lambda element: element.total_score/element.nb_visit).action
+        action = max(self.root.childrens, key=lambda element:self.side*element.total_score/element.nb_visit).action
         action.do(board=board)
     
